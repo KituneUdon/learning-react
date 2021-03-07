fba090740c642e56ad68dd3f40d414547bc70989
KituneUdon <kituneudon97@gmail.com>
2021-03-07 03:01:44 +0000

    #10 ログアウト機能を実装した

--- login-sample/frontend/src/components/Mypage.tsx
+++ login-sample/frontend/src/components/Mypage.tsx
@@ -1,18 +1,34 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import mypage from '../apis/mypage';
+import logout from '../apis/logout';
+import { AuthContext } from './Auth';
 
 const Mypage = () => {
   const [text, setText] = useState("");
+  const { setIsLogin } = useContext(AuthContext);
+
+  const history = useHistory();
 
   useEffect(() => {
     mypage().then((res) => setText(res));
   }, [setText])
 
+  const handleLogout = () => {
+    logout().then((value) => {
+      if (value) {
+        setIsLogin(false);
+        history.push("/");
+      }
+    })
+  }
+
   return (
-    <div>
-      {text}
-    </div>
+    <>
+      <p>{text}</p>
+      <button onClick={handleLogout}>ログアウト</button>
+    </>
   )
 }
 
